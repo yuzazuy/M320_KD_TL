f47e224b524ceba6a038c2b1465a0cd1d5187fd2
Kara <kara.diggelmann@edu.tbz.ch>
2026-08-18 16:44:46 +0200

    file structure fixed

---
+++ D1_Heizung_Aufgabe/src/ch/Main.java
@@ -0,0 +1,22 @@
+package ch;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+//        Heater myObj = new Heater(19, 0, 35, 1);
+//        myObj.userInput();
+
+
+        Heater myObj2 = new Heater(20, 3, 40, 10);
+        myObj2.userInput();
+
+
+    }
+}
+
